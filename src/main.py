10caa36741411d7b7be3b043ddd2f165cf03aa3f
nicolas ROCHE <nico_roche@hotmail.fr>
2020-04-13 13:52:23 +0200

    add load dataset

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
-from src.models import linear_sparse
 
-#linear_sparse()
+from src.utils.data import load_dataset
+
+dataset = load_dataset("..\\..\\dataset\\train_resized\\", 30000)
+
+
+print(dataset)
--- src/utils/data.py
+++ src/utils/data.py
@@ -1,6 +1,24 @@
 import glob
+import numpy as np
 from PIL import Image
 
+def load_dataset(path, size):
+    jpg_files_list = get_all_files_path(path)
+    dataset = np.ndarray(shape=(len(jpg_files_list), size), dtype=float)
+    i = 0
+    for file in jpg_files_list:
+        try:
+            im = Image.open(path + file)
+            pixels = list(im.getdata())
+            img = np.asarray(pixels, dtype="float32").flatten()
+            dataset[i] = img / 255
+            print(np.shape(dataset))
+            i = i + 1
+            if i == 100:
+                break
+        except IOError:
+            print('cannot open', file)
+    return dataset
 
 def get_all_files_path(path):
     jpg_files = []
@@ -21,7 +39,7 @@ def resize_save_images(jpg_files_list, size_x, size_y, path_folder, new_path_fol
             print('cannot open', file)
 
 
-path = "..\\..\\..\\dataset\\train\\"
-new_path = "..\\..\\..\\dataset\\train_resized\\"
-jpg_files = get_all_files_path(path)
-resize_save_images(jpg_files, 100, 100, path, new_path)
+# path = "..\\..\\..\\dataset\\train\\"
+# new_path = "..\\..\\..\\dataset\\train_resized\\"
+# jpg_files = get_all_files_path(path)
+# resize_save_images(jpg_files, 100, 100, path, new_path)
