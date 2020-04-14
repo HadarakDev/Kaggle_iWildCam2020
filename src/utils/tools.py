89ce4b6294079fd4da7a0b742c0f95114b736051
Uranium2 <tavernier.antoine@hotmail.fr>
2020-04-14 10:58:08 +0200

    try expect file not found

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -47,8 +47,11 @@ def move_img_to_category_folder(path_json_img, path_categories):
         data = json.load(json_file1)
         data2 = json.load(json_file2)
         for f in data["annotations"]:
-            img_name = f["image_id"] + ".jpg"
-            category = f["category_id"]
-            dir = next(item for item in data2["categories"] if item["id"] == category)["name"]
-            os.rename("../../dataset/train_resized/" + img_name, "../../dataset/train_resized/{}/{}".format(dir, img_name))
-            print(dir)
+            try:
+                img_name = f["image_id"] + ".jpg"
+                category = f["category_id"]
+                dir = next(item for item in data2["categories"] if item["id"] == category)["name"]
+                os.rename("../../dataset/train_resized/" + img_name, "../../dataset/train_resized/{}/{}".format(dir, img_name))
+                print(dir)
+            except IOError:
+                print('cannot open', dir)
