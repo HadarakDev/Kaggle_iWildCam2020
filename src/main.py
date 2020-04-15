94387aaf4eb79e60b7f09832c78549a2d2ca62da
Uranium2 <tavernier.antoine@hotmail.fr>
2020-04-15 11:17:58 +0200

    Fix shape for linear model

--- src/main.py
+++ src/main.py
@@ -9,18 +9,18 @@ import matplotlib as plt
 import numpy as np
 
 if __name__ == '__main__':
-    #X_all = load_dataset_pool("..\\..\\dataset\\train_resized\\", 10)
-    #print(X_all)
+    # X_all = load_dataset_pool("..\\..\\dataset\\train_resized\\", 10)
+    # print(X_all)
     # create_folder_from_categories("..\\categories.json")
     # move_img_to_category_folder("..\\annotations\\iwildcam2020_train_annotations.json", "..\\categories.json")
     batch = 100
-    number_of_classes = 2
+    number_of_classes = 216
     size_image_flatten = 30000
-    train_dataset, STEPS_PER_EPOCH = generate_dataset(path="E:\\dataset\\test\\", x=100, y=100, batch_size=batch)
+    train_dataset, STEPS_PER_EPOCH = generate_dataset(path="E:\\dataset\\train_resized\\", x=100, y=100,
+                                                      batch_size=batch)
 
-
-    model = linear(train_dataset, number_of_classes, size_image_flatten, "relu", "adam", "categorical_crossentropy", 2, batch, 0.02, STEPS_PER_EPOCH)
-
+    model = linear(train_dataset, number_of_classes, size_image_flatten, "relu", "adam", "categorical_crossentropy", 2,
+                   batch, 0.02, STEPS_PER_EPOCH)
 
     # for X, Y in train_dataset:
     #     start_time = time.time()
@@ -33,9 +33,3 @@ if __name__ == '__main__':
 # print(len(X_all))
 # print(len(Y))
 # nn_sparse(X_all, Y, 3, "selu", "adam", "categorical_crossentropy", 500, 1000, 0.0001, "./", "./", [512, 512, 512, 512], 0.2, 0, 0)
-
-
-
-
-
-
